3bbe1822f4c7c9dbc9d296ef16e39c3c972b4db8
Björn <5106696+b-gehrke@users.noreply.github.com>
2024-06-13 11:43:51 +0200

    Added performance evaluation source

--- test/performance/horned-perf/src/main.rs
+++ test/performance/horned-perf/src/main.rs
@@ -1,36 +1,36 @@
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::{Path, PathBuf};
 use std::time::{Duration, Instant};
+
 use horned_owl::io::ParserConfiguration;
-use horned_owl::io::rdf::reader::{RcRDFOntology, RDFOntology};
-use horned_owl::model::{AnnotatedComponent, Build, MutableOntology, RcStr};
-use horned_owl::ontology::set::{SetIndex, SetOntology};
+use horned_owl::io::rdf::reader::RcRDFOntology;
+use horned_owl::model::{Build, MutableOntology, RcStr};
 use horned_owl::model::SubClassOf;
-use horned_owl::ontology::declaration_mapped::DeclarationMappedIndex;
-use horned_owl::ontology::indexed::ThreeIndexedOntology;
-use horned_owl::ontology::logically_equal::LogicallyEqualIndex;
+use horned_owl::ontology::iri_mapped::RcIRIMappedOntology;
+use horned_owl::ontology::set::SetOntology;
 
 fn perf(path: &str) {
     println!("===={}====", path);
 
     println!("Opening...");
 
-    let mut o: Option<RcRDFOntology> = None;
+    let file = File::open(&path).unwrap();
+    let mut o: RcIRIMappedOntology = RcIRIMappedOntology::new_rc();
     let start = Instant::now();
     for _i in 0..3 {
-        let file = File::open(path).ok().unwrap();
-        let mut f = BufReader::new(file);
-        let b = horned_owl::model::Build::new_rc();
-        o = Some(horned_owl::io::rdf::reader::read_with_build(&mut f, &b, ParserConfiguration::default()).unwrap().0);
+        let mut f = BufReader::new(&file);
+        o = SetOntology::<RcStr>::from(horned_owl::io::rdf::reader::read(&mut f, ParserConfiguration::default()).unwrap().0).into();
     }
-    println!("Opened in {:.4?}s", start.elapsed() / 3);
+    println!("Opened in {:.4?}", start.elapsed() / 3);
 
     println!("Iterating...");
-    let o = o.unwrap();
+    let o = o;
     let mut time: Duration = Duration::default();
-    let c: SetOntology<RcStr> = o.into();
+    let c: RcIRIMappedOntology = o.into();
     for _i in 0..3 {
-        let c = c.clone();
+        let c = c.iter().clone();
         let mut amount = 0;
         let start2 = Instant::now();
         for a in c.into_iter() {
@@ -40,20 +40,22 @@ fn perf(path: &str) {
         }
         time += start2.elapsed();
     }
-    println!("Iterated in {:.4?}s", time / 3);
+    println!("Iterated in {:.4?}", time / 3);
 
     println!("Adding 100.000 axioms");
     let mut c = SetOntology::<RcStr>::default();
     let b = Build::new_rc();
     let start3 = Instant::now();
-    for i in 0..100_000{
-        let a = SubClassOf::new(b.class( format!("https://example.com/entity_{}", i)), b.class( format!("https://example.com/entity_{}", i)));
-        c.insert(a)
+    for i in 0..100_000 {
+        let a = SubClassOf::new(
+            b.class(format!("https://example.com/entity_{}", i)).into(),
+            b.class(format!("https://example.com/entity_{}", i)).into());
+        c.insert(a);
     }
     println!("Added 100.000 axioms in {:.4?}", start3.elapsed())
 }
 
 fn main() {
-    perf("go.owl");
-    perf("chebi_core.owl");
+    perf("../go.owl");
+    perf("../chebi_core.owl");
 }
