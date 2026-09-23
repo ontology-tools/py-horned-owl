7dff81ef10dac0a8dfba991066d4895037907c75
Björn <5106696+b-gehrke@users.noreply.github.com>
2026-09-23 16:13:43 +0200

    fix: update tests to user structural reasoner instead of deprecated methods

--- test/test_hierarchy.py
+++ test/test_hierarchy.py
@@ -1,6 +1,8 @@
 import pytest
 from test_base import OWL_NOTHING, OWL_THING, simple_ontology
 
+from pyhornedowl.reasoning import create_structural_reasoner
+
 
 def test_no_subclass():
     o = simple_ontology()
@@ -40,59 +42,73 @@ def test_direct_superclass():
 
 def test_no_ancestors():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
     expected = set()
-    actual = o.get_ancestors(":A")
+    actual = r.get_superclasses(o.class_(":A"))
 
     assert expected == actual
 
 
 def test_no_descendants():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
     expected = set()
-    actual = o.get_descendants(":C")
+    actual = r.get_subclasses(o.class_(":C"))
 
     assert expected == actual
 
 
 def test_single_ancestors():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
-    expected = {"https://example.com/A"}
-    actual = o.get_ancestors(":B")
+    expected = {o.class_("https://example.com/A")}
+    actual = r.get_superclasses(o.class_(":B"))
 
     assert expected == actual
 
 
 def test_single_descendants():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
-    expected = {"https://example.com/D"}
-    actual = o.get_descendants(":B")
+    expected = {o.class_("https://example.com/D")}
+    actual = r.get_subclasses(o.class_(":B"))
 
     assert expected == actual
 
 
 def test_multiple_ancestors():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
-    expected = {"https://example.com/A", "https://example.com/B", OWL_THING}
-    actual = o.get_ancestors(":D")
+    expected = {
+        o.class_("https://example.com/A"),
+        o.class_("https://example.com/B"),
+        o.class_(OWL_THING),
+    }
+    actual = r.get_superclasses(o.class_(":D"))
 
     assert expected == actual
 
 
 def test_multiple_descendants():
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
-    expected = {"https://example.com/B", "https://example.com/D"}
-    actual = o.get_descendants(":A")
+    expected = {
+        o.class_("https://example.com/B"),
+        o.class_("https://example.com/D"),
+    }
+    actual = r.get_subclasses(o.class_(":A"))
 
     assert expected == actual
 
 
 # owl:Thing tests (top-level class)
+
 
 def test_subclasses_of_thing():
     """owl:Thing subclasses should not do inference and only return subclasses explicitly declared as subclasses of owl:Thing"""
@@ -118,9 +134,10 @@ def test_superclasses_of_thing():
 def test_ancestors_of_thing():
     """owl:Thing has no ancestors"""
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
     expected = set()
-    actual = o.get_ancestors(OWL_THING)
+    actual = r.get_superclasses(o.class_(OWL_THING))
 
     assert expected == actual
 
@@ -128,19 +145,21 @@ def test_ancestors_of_thing():
 def test_descendants_of_thing():
     """owl:Thing descendants should return all classes"""
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
     expected = {
-        "https://example.com/A",
-        "https://example.com/B",
-        "https://example.com/C",
-        "https://example.com/D",
+        o.class_("https://example.com/A"),
+        o.class_("https://example.com/B"),
+        o.class_("https://example.com/C"),
+        o.class_("https://example.com/D"),
     }
-    actual = o.get_descendants(OWL_THING)
+    actual = r.get_subclasses(o.class_(OWL_THING))
 
     assert expected == actual
 
 
 # owl:Nothing tests (bottom-level class)
+
 
 def test_subclasses_of_nothing():
     """owl:Nothing has no subclasses"""
@@ -165,9 +184,10 @@ def test_superclasses_of_nothing():
 def test_ancestors_of_nothing():
     """owl:Nothing ancestors - bottom Element of the hierarchy, should have all other classes as ancestors"""
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
-    expected = o.get_classes()
-    actual = o.get_ancestors(OWL_NOTHING)
+    expected = {o.class_(c) for c in o.get_classes()}
+    actual = r.get_superclasses(o.class_(OWL_NOTHING))
 
     assert expected == actual
 
@@ -175,11 +195,13 @@ def test_ancestors_of_nothing():
 def test_descendants_of_nothing():
     """owl:Nothing has no descendants"""
     o = simple_ontology()
+    r = create_structural_reasoner(o)
 
     expected = set()
-    actual = o.get_descendants(OWL_NOTHING)
+    actual = r.get_subclasses(o.class_(OWL_NOTHING))
 
     assert expected == actual
+
 
 def test_root_classes():
     o = simple_ontology()
