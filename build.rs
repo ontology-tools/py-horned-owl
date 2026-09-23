b395f0f596ee21ef0ede31ccf433488f29bfec7d
Björn <5106696+b-gehrke@users.noreply.github.com>
2026-09-23 16:51:38 +0200

    fix: improve build time

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ use std::env;
 
 pub fn main() {
     println!("cargo::rustc-check-cfg=cfg(pyi)");
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-env-changed=INCLUDE_PYI");
     if let Ok(flag) = env::var("INCLUDE_PYI") {
         if flag == "1" || flag.to_lowercase() == "true" {
             println!("cargo:rustc-cfg=pyi");
